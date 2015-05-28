b5406112ed4809995a4b89602541e0fe9199fab5
m47bell <m.vidals047@gmail.com>
2015-05-28 19:44:05 -0400

    initial commit

---
+++ src/MyObjectNode.java
@@ -0,0 +1,45 @@
+/**
+ * Created by c4q-marbella on 5/28/15.
+ * Access Code 2-1
+ * Marbella Vidals
+ */
+public abstract class MyObjectNode extends MyNode
+{
+    @Override
+    public MyNode getLeft()
+    {
+        return null;
+    }
+
+    @Override
+    public MyNode getRight()
+    {
+        return null;
+    }
+
+    @Override
+    public Object getData()
+    {
+        return null;
+    }
+
+    public void setRight(){
+
+    }
+
+    public void setLeft(){
+
+    }
+
+    public void setData(){
+
+    }
+    @Override
+    public void insert(MyNode newNode)
+    {
+
+    }
+
+
+
+}
