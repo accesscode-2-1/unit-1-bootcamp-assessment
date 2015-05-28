4ee63b643be0886af1d8c52e9dc5fab2527aaf88
ramonaharrison <ramona.e.harrison@gmail.com>
2015-05-28 19:55:08 -0400

    Revert "finished"
    This reverts commit 94265f18c8989bf498a43bd47e010fb80e119bf6.

--- src/Exercises.java
+++ src/Exercises.java
@@ -1,11 +1,11 @@
 public class Exercises {
 
     static class Parent {
-        public String doStuff() { return "parent"; }
+        public String doStuff() { return ""; }
     }
     static class Child extends Parent {
-        public String doStuff() { return "child"; }
-        public String doStuff(String s) { return s; }
+        public String doStuff() { return ""; }
+        public String doStuff(String s) { return ""; }
     }
 
 
