890db13330d39bbaff8e2a06cf5c5e5a5b681e50
theaulait <vanice.yee@gmail.com>
2015-05-28 21:59:43 -0400

    initial

--- README.md
+++ README.md
@@ -23,7 +23,7 @@
 
 Test Start time: Thu May 28, 7:00pm EST
 
-My End time: `<insert here>`
+My End time: `<>`
 
 Test End time: Fri May 29, 10:00pm EST
 
---
+++ src/MyObjectNode.java
@@ -0,0 +1,41 @@
+/**
+ * Created by c4q-vanice on 5/28/15.
+ */
+public class MyObjectNode extends MyNode {
+
+    public void setLeft(MyNode left){
+        this.left = left;
+    }
+
+    @Override
+    public MyNode getLeft()
+    {
+        return null;
+    }
+
+    public void setRight(MyNode right){
+        this.right = right;
+    }
+
+    @Override
+    public MyNode getRight()
+    {
+        return null;
+    }
+
+    public void setData(MyNode data){
+        this.data = data;
+    }
+
+    @Override
+    public Object getData()
+    {
+        return null;
+    }
+
+    @Override
+    public void insert(MyNode newNode)
+    {
+
+    }
+}
