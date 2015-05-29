7b62d09a8bea56739d22ec0100f4390daaf72bdf
Jorge Reina <jorgereina1986@gmail.com>
2015-05-28 20:25:52 -0400

    test complete

---
+++ src/MyObjectNode.java
@@ -0,0 +1,45 @@
+/**
+ * Created by Jorge Reina on 5/28/15.
+ */
+public class MyObjectNode extends MyNode
+{
+    public MyObjectNode setLeft()
+    {
+        return null;
+    }
+
+    public MyObjectNode setRight()
+    {
+        return null;
+    }
+
+    public MyObjectNode setData()
+    {
+        return null;
+    }
+
+    @Override
+    public MyNode getLeft()
+    {
+        return null;
+    }
+
+    @Override
+    public MyNode getRight()
+    {
+        return null;
+    }
+
+    @Override
+    public Object getData()
+    {
+        return null;
+    }
+
+    @Override
+    public void insert(MyNode newNode)
+    {
+
+    }
+
+}
